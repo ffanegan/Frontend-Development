319c50e46c079a5abfc385572bbb036b4d52b646
ffanegan <ffanegan@uchicago.edu>
2024-08-26 15:34:59 -0500

    Committing Second Version

--- Tip/script.js
+++ Tip/script.js
@@ -1,54 +1,24 @@
-let display = document.getElementById("display");
-let bill = document.getElementById("bill");
-let tip = document.getElementById("tip");
-// const go = document.getElementById("go");
-
-// function appendToDisplay(input){
-//     display.value = total;
-// }
-
-// go.addEventListener("click", calculate);
-
-
-function dollarsign(input){
-    // document.getElementById("bill").value = "$" + input;
-    document.getElementById("bill").value = `$${input}`;
-}
-
 function clearDisplay(){
-    document.getElementById("display").value = "";
     document.getElementById("bill").value = "";
     document.getElementById("tip").value = "";
     document.getElementById("tip_display").value = "";
+    document.getElementById("display").value = "";
 }
 
 function calculate(){
-    bill = parseInt(document.getElementById("bill").value);
-    tip = parseInt(document.getElementById("tip").value);
-    var display = document.getElementById("display").value;
+    //brings in element IDs and gets their value
+    const bill = parseFloat(document.getElementById("bill").value);
+    const tip = parseFloat(document.getElementById("tip").value);
+    //Converts tip into percentage
     x = tip * 0.01;
-    document.getElementById("tip_display").value = `$${(x * (bill*10))/10}`;
-    total_value = bill + ((bill*10) * x)/10
-    document.getElementById("display").value = `$${total_value.toFixed(2)}`;
+    //Only calculate the tip (tip% * bill)
+    document.getElementById("tip_display").value = `$${(x * (bill)).toFixed(2)}`;
+    //Finally calculates the total with tip
     try{
-        document.getElementById("display").value = `$${bill + (bill * x)}`;
+        total_value = bill + ((bill*10) * x)/10;
     }
-    catch{
+    catch(NaN){
         document.getElementById("display").value = "Error";
     }
+    document.getElementById("display").value = `$${total_value.toFixed(2)}`;
 }
-
-console.log((256.2399.toFixed(2)))
-
-// function calculate(){
-//     let bill = document.getElementById("bill");
-//     let tip = document.getElementById("tip");
-//     try{
-//         display.value = (bill.value * tip.value) + bill.value
-//     }
-//     catch(error){
-//         display = "Error";
-//     }
-// }
-
-// console.log((100 * 0.15) + 100)
